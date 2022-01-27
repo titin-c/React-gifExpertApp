9d8519f524e46c76ed538554875c58cd7ce8cc73
Your NameTitín <titin.colomer.romero@gmail.com>
2022-01-27 11:23:23 +0100

    Docs añadido

---
+++ docs/precache-manifest.75d990be8d616e5ae09ee2ddb5b500b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b4c1d0b5617f79a1aed13880144846",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7afc30966145c953d798",
+    "url": "/static/css/main.a9cc5714.chunk.css"
+  },
+  {
+    "revision": "245c5c306f4866cef443",
+    "url": "/static/js/2.928b70a7.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.928b70a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7afc30966145c953d798",
+    "url": "/static/js/main.6a49b6b5.chunk.js"
+  },
+  {
+    "revision": "c0aee2adc1c6652c7ce1",
+    "url": "/static/js/runtime-main.579092b4.js"
+  }
+]);
